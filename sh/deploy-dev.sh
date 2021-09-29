c9003b69e3c1899a29062ee395b39282a70b789e
蒋金 <ginlink@163.com>
2021-09-29 17:42:14 +0800

    u

--- sh/deploy-dev.sh
+++ sh/deploy-dev.sh
@@ -1,7 +1,8 @@
+#!/bin/sh
 ###
  # @Author: jiangjin
  # @Date: 2021-09-29 11:41:09
- # @LastEditTime: 2021-09-29 15:41:53
+ # @LastEditTime: 2021-09-29 17:42:04
  # @LastEditors: jiangjin
  # @Description: 
  # 
@@ -13,12 +14,11 @@ echo "$DEPLOY_KEY" > deploy.key
 mkdir -p ~/.ssh
 chmod 0600 deploy.key && ssh-add deploy.key
 echo -e "Host *\n\tStrictHostKeyChecking no\n\n" > ~/.ssh/config
-ssh-add deploy.key
 
 # 2.拷贝自动化脚本、docker compose配置文件到服务器
-scp -P 19338 ./sh/server-re-deploy-dev.sh root@24t6651n59.wicp.vip:~/sh/
-scp -P 19338 ./docker-compose-dev.yml root@24t6651n59.wicp.vip:~/sh/
+# scp -P 19338 ./sh/server-re-deploy-dev.sh root@24t6651n59.wicp.vip:~/server-re-deploy-dev.sh
+# scp -P 19338 ./docker-compose-dev.yml root@24t6651n59.wicp.vip:~/docker-compose-dev.yml
 
 # 3.登录服务器，并执行自动化脚本
 ssh root@24t6651n59.wicp.vip -p 19338
-cd sh && sh server-re-deploy-dev.sh
+# cd sh && sh server-re-deploy-dev.sh
--- sh/server-re-deploy-dev.sh
+++ sh/server-re-deploy-dev.sh
@@ -1,15 +1,15 @@
-
+#!/bin/sh
 ###
  # @Author: jiangjin
  # @Date: 2021-09-29 14:27:53
- # @LastEditTime: 2021-09-29 14:31:33
+ # @LastEditTime: 2021-09-29 17:29:13
  # @LastEditors: jiangjin
  # @Description: 
  # 
 ### 
  
 # 拉取镜像，重新启动服务
-docker login -u $DOCKER_ACCESS_KEY -p $DOCKER_ACCESS_TOKEN
+docker login -u "$DOCKER_ACCESS_KEY" -p "$DOCKER_ACCESS_TOKEN"
 
 docker rmi -f ginlink/sheep-web:dev
 docker-compose -f docker-compose.dev.yml pull web

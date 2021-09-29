99016232c056a4daebe8027ea0e46edcf6a6c824
蒋金 <ginlink@163.com>
2021-09-29 16:46:23 +0800

    u

--- sh/build-dev.sh
+++ sh/build-dev.sh
@@ -2,7 +2,7 @@
 ###
  # @Author: jiangjin
  # @Date: 2021-09-29 14:34:03
- # @LastEditTime: 2021-09-29 16:39:18
+ # @LastEditTime: 2021-09-29 16:46:08
  # @LastEditors: jiangjin
  # @Description: 
  # 
@@ -11,7 +11,9 @@
 # 1.打包
 yarn && yarn build
 
-echo "$DOCKER_ACCESS_TOKEN" | docker login --username "$DOCKER_ACCESS_KEY" --password-stdin
+# echo "$DOCKER_ACCESS_TOKEN" | docker login --username "$DOCKER_ACCESS_KEY" --password-stdin
+
+docker login --username "ginlink" -p 'da44ef14-406b-46a6-a102-5d6dbf0509e0'
 
 # 2.打包成docker镜像（nginx）
 docker build -t sheep-web:dev .

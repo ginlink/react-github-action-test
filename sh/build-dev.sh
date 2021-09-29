a41ae775b5daeed577419d5ea2683b7060188c5a
蒋金 <ginlink@163.com>
2021-09-29 16:07:07 +0800

    u

--- sh/build-dev.sh
+++ sh/build-dev.sh
@@ -2,7 +2,7 @@
 ###
  # @Author: jiangjin
  # @Date: 2021-09-29 14:34:03
- # @LastEditTime: 2021-09-29 15:35:00
+ # @LastEditTime: 2021-09-29 16:07:02
  # @LastEditors: jiangjin
  # @Description: 
  # 
@@ -12,8 +12,9 @@
 yarn && yarn build
 
 # 2.打包成docker镜像（nginx）
-docker build -t ginlink/sheep-web:dev .
+docker build -t sheep-web:dev .
+docker tag sheep-web:dev ginlink/pr:sheep-web
 
 # 3.推送到dockerhub
 docker login -u "$DOCKER_ACCESS_KEY" -p "$DOCKER_ACCESS_TOKEN"
-docker push ginlink/sheep-web:dev
+docker push ginlink/pr:sheep-web
--- sh/deploy-dev.sh
+++ sh/deploy-dev.sh
@@ -1,7 +1,7 @@
 ###
  # @Author: jiangjin
  # @Date: 2021-09-29 11:41:09
- # @LastEditTime: 2021-09-29 14:45:37
+ # @LastEditTime: 2021-09-29 15:41:53
  # @LastEditors: jiangjin
  # @Description: 
  # 
@@ -9,12 +9,16 @@
 
 # 1.添加ssh private key(为连接服务器)，并配置ssh
 eval $(ssh-agent -s)
-echo ${secrets.DEPLOY_KEY} > deploy.key
+echo "$DEPLOY_KEY" > deploy.key
 mkdir -p ~/.ssh
 chmod 0600 deploy.key && ssh-add deploy.key
 echo -e "Host *\n\tStrictHostKeyChecking no\n\n" > ~/.ssh/config
+ssh-add deploy.key
 
 # 2.拷贝自动化脚本、docker compose配置文件到服务器
-# scp ./sh/server-re-deploy-dev.sh root@
+scp -P 19338 ./sh/server-re-deploy-dev.sh root@24t6651n59.wicp.vip:~/sh/
+scp -P 19338 ./docker-compose-dev.yml root@24t6651n59.wicp.vip:~/sh/
 
 # 3.登录服务器，并执行自动化脚本
+ssh root@24t6651n59.wicp.vip -p 19338
+cd sh && sh server-re-deploy-dev.sh

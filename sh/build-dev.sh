1541f04cce8f8df17b6a73af88a9862c432c0aa5
蒋金 <ginlink@163.com>
2021-09-29 16:55:14 +0800

    u

--- sh/build-dev.sh
+++ sh/build-dev.sh
@@ -2,7 +2,7 @@
 ###
  # @Author: jiangjin
  # @Date: 2021-09-29 14:34:03
- # @LastEditTime: 2021-09-29 16:54:02
+ # @LastEditTime: 2021-09-29 16:55:10
  # @LastEditors: jiangjin
  # @Description: 
  # 
@@ -14,8 +14,7 @@
 # echo "$DOCKER_ACCESS_TOKEN" | docker login --username "$DOCKER_ACCESS_KEY" --password-stdin
 
 # docker login --username "ginlink" -p 'da44ef14-406b-46a6-a102-5d6dbf0509e0'
-echo "--------${{ secrets.DOCKER_ACCESS_KEY }}-------------/n"
-echo "----------${{ secrets.DOCKER_ACCESS_TOKEN }}-------"
+echo "--------$secrets-------------/n"
 
 
 
